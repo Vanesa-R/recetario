81529d98162ba4fbc21916fe7a4dfb1447f2fd2d
Vanesa <uve.rdguez@gmail.com>
2021-09-28 16:00:11 +0200

    Estilo de botones de búsqueda y funcionalidad de búsqueda por voz

--- app/js/searchRecipes.js
+++ app/js/searchRecipes.js
@@ -8,7 +8,17 @@ const btnSearch = document.querySelector("#search__recipe");
 const showRecipe = document.querySelector(".section__recipe")
 const card = document.querySelector(".card__recipe")
 
+
+// Poner el foco en el input de texto 
+
+inputRecipe.focus()
+
+
 // Habilitar y deshabilitar el buscador
+
+inputRecipe.addEventListener("input", (e) => {
+        (inputRecipe.value === "") ? disabledBtnSearch() : enabledBtnSearch();
+})
 
 const enabledBtnSearch = () => {
         btnSearch.disabled = false;
@@ -19,18 +29,108 @@ const disabledBtnSearch = () => {
         clean()
 }
 
-inputRecipe.addEventListener("input", (e) => {
-        (inputRecipe.value === "") ? disabledBtnSearch() : enabledBtnSearch();
+
+
+
+// Si el botón está habilitado,
+// búsqueda en la base de datos  e impresión de receta en la pantalla
+
+btnSearch.addEventListener("click", (e) => {
+        printRecipe()
 })
+
+inputRecipe.addEventListener("change", (e) => {
+        printRecipe()
+})
+
+const printRecipe = () => {
+
+        fetch("../dist/js/recipes.json")
+        .then(res => res.ok ? Promise.resolve(res) : Promise.reject(error))
+        .then(res => res.json())
+        .then(data => {
+                
+                const fragment = document.createDocumentFragment();
+
+                if (showRecipe.textContent === "") {
+
+                        for (const key of data){
+
+                                if (key.title
+                                        .toLowerCase()
+                                        .normalize("NFD").replace(/[\u0300-\u036f]/g, '')
+                                        .includes(inputRecipe.value
+                                                .toLowerCase()
+                                                .normalize("NFD").replace(/[\u0300-\u036f]/g, ''))){         
+                                        
+                                                       
+                                        // Card de la receta
+                                        const card = document.createElement("div");
+                                        card.classList.add("card__recipe");
+
+
+                                        // Título
+                                        const title = document.createElement("h1");
+                                        title.classList.add("title__recipe");
+                                        title.textContent = `${key.title}`
+                                        card.appendChild(title);
+
+
+                                        // Ingredientes
+                                        const subtitleIngredients = document.createElement("h2");
+                                        subtitleIngredients.classList.add("subtitle__recipe");
+                                        subtitleIngredients.textContent = "Ingredientes:"
+                                        card.appendChild(subtitleIngredients);
+
+                                        const listIngredients = document.createElement("ul");
+                                        listIngredients.classList.add("list__ingredients");
+                                        
+                                        for (let i of key.ingredients){
+                                                const itemIngredients = document.createElement("li");
+                                                itemIngredients.classList.add("list__item__ingredients");
+                                                itemIngredients.textContent = `${i}`
+                                                listIngredients.appendChild(itemIngredients)
+                                        }
+                                        card.appendChild(listIngredients)
+
+
+                                        // Elaboración
+
+                                        const subtitleElaboration = document.createElement("h2");
+                                        subtitleElaboration.classList.add("subtitle__recipe");
+                                        subtitleElaboration.textContent = "Elaboración:"
+                                        card.appendChild(subtitleElaboration);
+
+                                        const sectionElaboration = document.createElement("div");
+                                        sectionElaboration.classList.add("elaboration__recipe");
+
+                                        for (let p of key.elaboration){
+                                                const elaboration = document.createElement("p");
+                                                elaboration.classList.add("paragraph__elaboration");
+                                                elaboration.textContent = `${p}`
+                                                sectionElaboration.appendChild(elaboration)
+                                        }
+
+                                        card.appendChild(sectionElaboration)
+
+                                        fragment.appendChild(card)
+
+                                }
+                        
+                                showRecipe.appendChild(fragment)
+        
+                        }
+                }
+
+                
+        })
+        .catch(err => console.log(err))
+}
+
+
 
 
 // Limpiar sección de recetas
-
-const clean = () => {
-        showRecipe.textContent = ""
-        showRecipe.removeChild(card)
-}
-
 
 inputRecipe.addEventListener("input", (e) => {
         if  ((inputRecipe.value === "") && (showRecipe.textContent !== "")){
@@ -38,89 +138,7 @@ inputRecipe.addEventListener("input", (e) => {
         }
 })
 
-// Evento de escucha sobre el buscador si está habilitado
-
-btnSearch.addEventListener("click", (e) => {
-
-        fetch("../dist/js/recipes.json")
-                .then(res => res.ok ? Promise.resolve(res) : Promise.reject(error))
-                .then(res => res.json())
-                .then(data => {
-                        
-                        const fragment = document.createDocumentFragment();
-
-                        if (showRecipe.textContent === "") {
-
-                                for (const key of data){
-
-                                        if (key.title
-                                                .toLowerCase()
-                                                .normalize("NFD").replace(/[\u0300-\u036f]/g, '')
-                                                .includes(inputRecipe.value
-                                                        .toLowerCase()
-                                                        .normalize("NFD").replace(/[\u0300-\u036f]/g, ''))){         
-                                                
-                                                               
-                                                // Card de la receta
-                                                const card = document.createElement("div");
-                                                card.classList.add("card__recipe");
-
-
-                                                // Título
-                                                const title = document.createElement("h1");
-                                                title.classList.add("title__recipe");
-                                                title.textContent = `${key.title}`
-                                                card.appendChild(title);
-
-
-                                                // Ingredientes
-                                                const subtitleIngredients = document.createElement("h2");
-                                                subtitleIngredients.classList.add("subtitle__recipe");
-                                                subtitleIngredients.textContent = "Ingredientes:"
-                                                card.appendChild(subtitleIngredients);
-
-                                                const listIngredients = document.createElement("ul");
-                                                listIngredients.classList.add("list__ingredients");
-                                                
-                                                for (let i of key.ingredients){
-                                                        const itemIngredients = document.createElement("li");
-                                                        itemIngredients.classList.add("list__item__ingredients");
-                                                        itemIngredients.textContent = `${i}`
-                                                        listIngredients.appendChild(itemIngredients)
-                                                }
-                                                card.appendChild(listIngredients)
-
-
-                                                // Elaboración
-
-                                                const subtitleElaboration = document.createElement("h2");
-                                                subtitleElaboration.classList.add("subtitle__recipe");
-                                                subtitleElaboration.textContent = "Elaboración:"
-                                                card.appendChild(subtitleElaboration);
-
-                                                const sectionElaboration = document.createElement("div");
-                                                sectionElaboration.classList.add("elaboration__recipe");
-
-                                                for (let p of key.elaboration){
-                                                        const elaboration = document.createElement("p");
-                                                        elaboration.classList.add("paragraph__elaboration");
-                                                        elaboration.textContent = `${p}`
-                                                        sectionElaboration.appendChild(elaboration)
-                                                }
-
-                                                card.appendChild(sectionElaboration)
-
-                                                fragment.appendChild(card)
-
-                                        }
-                                
-                                        showRecipe.appendChild(fragment)
-                
-                                }
-                        }
-
-                        
-                })
-                .catch(err => console.log(err))
-      
-})
+const clean = () => {
+        showRecipe.textContent = ""
+        showRecipe.removeChild(card)
+}
--- app/js/speechRecognition.js
+++ app/js/speechRecognition.js
@@ -1,14 +1,36 @@
-if (window.hasOwnProperty("SpeechRecognition") || window.hasOwnProperty("webkitSpeechRecognition")) {
-      
-        // Si la API está disponible creamos el objeto
-        const recognition = new webkitSpeechRecognition();
+/**************************
+     BUSCADOR por voz
+***************************/
 
-        // Propiedades
-        recognition.lang = 'es-ES';
-        recognition.interimResults = true;
-        recognition.continuous = false;
+//  Variables DOM
+const btnVoice = document.querySelector("#voice__search");
+
+
+let recognition = new webkitSpeechRecognition();
+
+// Propiedades
+recognition.lang = 'es-ES';
+recognition.continuous = false;
+recognition.interimResults = true;
+
+recognition.onresult = (event) => {
         
+        let results = event.results;
+       inputRecipe.value = results[results.length -1][0].transcript;
+
+       printRecipe()
+
+          
+}
+
+
+btnVoice.addEventListener("click", (e) => {
+        recognition.start()
+})
+
+
+
               
-} else {
-          console.log("La API Speech Recognition no está disponible")
+if (!(window.hasOwnProperty("SpeechRecognition") || window.hasOwnProperty("webkitSpeechRecognition"))) {
+        console.log("La API Speech Recognition no está disponible")
 }
